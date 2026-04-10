884405c6425a608cc03d1c9d3085adf429f296fc
kudoshinichi2k5 <23520797@gm.uit.edu.vn>
2026-04-10 16:12:21 +0700

    feat: add solution for prob2 in the first time

--- fibonacci.py
+++ fibonacci.py
@@ -1,32 +1,60 @@
-from multiprocessing import Pool
+import multiprocessing
 import time
 
+q = 1
+arr = []
+
 def fibonacci(n):
-    if n <= 1:
-        return n
-    else:
-        return fibonacci(n-1) + fibonacci(n-2)
+    a, b = 0, 1
+    for _ in range(n):
+        a, b = b, a+b
+        a %= q
+        b %= q
+    return a
 
+def func(bounds):
+    arr_tmp = []
+    start, end = bounds
+    
+    for i in range(start, end):
+        res = fibonacci(arr[i])
+        arr_tmp.append(res)
+    
+    return arr_tmp
+
+def MAIN(input_file_path):
+    global arr, q
+
+    with open(input_file_path, "r") as f:
+        first_line = f.readline().split()
+
+        n = int(first_line[0])
+        q = int(first_line[1])
+
+        arr = [int(f.readline()) for _ in range(n)]
+
+    num_cores = 2
+    chunk_size = (n + num_cores - 1) // num_cores
+
+    bounds = []
+    for i in range(num_cores):
+        start = i * chunk_size
+        end = min((i + 1) * chunk_size, n)
+        if start < end:
+            bounds.append((start, end))
+
+    with multiprocessing.Pool(num_cores) as p:
+        results = p.map(func, bounds)
+
+    final_results = []
+    for res in results:
+        final_results.extend(res)
+
+    return final_results  
+    
 if __name__ == "__main__":
-    n = int(input())
+    start = time.perf_counter()
+    print(MAIN("input.txt"))
 
-    start = time.time()
-
-    with Pool(4) as p:
-        results = p.map(fibonacci, range(n+1))
-    
-    end = time.time()
-    print(results)
-    print(f"Thời gian tính toán: {end - start:.4f} giây")
-
-    
-    start = time.time()
-
-    resultss = fibonacci(n)
-    
-    end = time.time()
-
-    print(resultss)
-    print(f"Thời gian tính toán: {end - start:.4f} giây")
-                
-
+    end = time.perf_counter()
+    print("Thoi gian chay: ", end - start)
